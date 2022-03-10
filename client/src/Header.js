8c3e1542a1f17218a218a5a638f6131dbd6ded4c
Alec Mitrokostas <amitroko@yahoo.com>
2022-03-09 22:35:42 -0500

    added connected address display

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,19 +1,21 @@
-import { providers } from 'ethers';
+import React, { useState } from 'react';
 import './styles/Header.css';
 import { ethers } from 'ethers';
 
 function Header() {
+    const [address, setAddress] = useState('');
     const connectWallet = async() => {
         if (!window.ethereum) console.log("No wallet found.")
         await window.ethereum.send("eth_requestAccounts");
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-        console.log(signer.getAddress());
+        const addr = await signer.getAddress();
+        setAddress(addr.substring(0, 6) + '...' + addr.substring(addr.length - 4));
     }
     return (
         <div className="header-wrapper">
             <h1>BigEye NFT</h1>
-            <button onClick={connectWallet}>connect</button>
+            { address ? <p class="address">Connected: {address}</p> : <button class="address" onClick={connectWallet}>Connect Wallet</button> }
         </div>
     )
 }
